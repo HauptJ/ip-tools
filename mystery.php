3b8e17eb4b7a2e2583e0dc796fe09c8113f3fd5a
Joshua Haupt <joshuanhaupt@outlook.com>
2019-05-10 09:32:37 -0500

    Revert "Renamed video page"

---
+++ mystery.php
@@ -0,0 +1,41 @@
+<?php
+//mystery.php
+//Josh
+//26.7.14
+//Desc: displays a video
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Trololo</title>
+        <script src="https://jwpsrv.com/library/3ADBggl8EeSvuyIACtqXBA.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+    <body>
+        
+        <?php 
+        //include the awsome nav bar
+        include("navBar.php"); 
+        ?>
+
+        <!--Play the funky music white boi-->
+        <div id ='main'>
+            <div id='playerAbAaKnKdHGmU'></div>
+            <script type='text/javascript'>
+                src="https://jwpsrv.com/library/3ADBggl8EeSvuyIACtqXBA.js";
+                jwplayer('playerAbAaKnKdHGmU').setup({
+                    file: '/videos/ed_hd.mp4',
+                    image: '/videos/ed_cover.jpg',
+                    title: 'Elephants Dream',
+                    width: '560',
+                    height: '315',
+                    autostart: 'false',
+                    repeat: 'false'
+                });
+            </script>
+        </div>
+
+    </body>
+</html>
